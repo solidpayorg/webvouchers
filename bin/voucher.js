4a136ae0be8af09fa93e82cd12cc3c47976b7038
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-21 23:29:39 +0200

    add voucher.js

---
+++ bin/voucher.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// parse command line
+const minimist = require('minimist')
+const fetch = require('node-fetch')
+
+const opts = minimist(process.argv.slice(2))
+const args = opts._
+
+const server = opts.server || 'https://localhost:5010'
+const cert = opts.cert
+
+// get channel balance
+fetch(server)
+  .then(res => res.text())
+  .then(body => console.log(body))
